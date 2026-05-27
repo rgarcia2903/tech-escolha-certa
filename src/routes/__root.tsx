d536e8de959a4cd76551aaf19eebed5224d81cdb
Ricardo Capistrano <rcapistrano.garcia@gmail.com>
2026-05-26 23:16:42 -0300

    Adiciona favicon do site

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -127,12 +127,21 @@ function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
   head: () => ({
-    links: [
-      {
-        rel: "stylesheet",
-        href: appCss,
-      },
-    ],
+      links: [
+        {
+          rel: "stylesheet",
+          href: appCss,
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          href: "/favicon.png",
+        },
+      ],
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
