d28a6ee0823839e035771a5a8780aafb2bde4dc4
Ricardo Capistrano <rcapistrano.garcia@gmail.com>
2026-06-01 22:45:40 -0300

    Corrige instalação do Microsoft Clarity

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,13 +24,18 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
 <head>
-  <script type="text/javascript">
-  (function(c,l,a,r,i,t,y){
-      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-  })(window, document, "clarity", "script", "x0ds1dhbb4");
-</script>
+  <script
+    type="text/javascript"
+    dangerouslySetInnerHTML={{
+    __html: `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "x0ds1dhbb4");
+    `,
+    }}
+/>
   <HeadContent />
 
   <script
